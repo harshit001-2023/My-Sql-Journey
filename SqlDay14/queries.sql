d637107e234be9a3914ee9240ae1a562e4b4c4e3
harshit001-2023 <harshitpande938@gmail.com>
2025-11-14 17:31:07 +0530

    Completing JOIN queries day 2 [13/11/2025]

--- SqlDay14/queries.sql
+++ SqlDay14/queries.sql
@@ -20,6 +20,25 @@ WHERE E.SAL > A.SAL;
 
 --4.Write a query to display BLAKE'S manager name from emp table
 --  by using self join?
+SELECT m.ENAME AS "BLAKE'S Manager"
+FROM emp e
+JOIN emp m ON e.MGR = m.EMPNO
+WHERE e.ENAME = 'BLAKE';
 
+--5.Write a query to display the employees who are joining
+--before their managers from emp table
+--by using self join?
+SELECT e.EMPNO, e.ENAME, e.HIREDATE, m.EMPNO, m.ENAME,m.HIREDATE
+FROM emp e
+JOIN emp m ON e.MGR = m.EMPNO
+WHERE e.HIREDATE < m.HIREDATE
+ORDER BY e.HIREDATE;
 
+--6. Write a query to display the employees who are getting same salary
+--    as SCOTT salary from emp table using self join?
+SELECT e2.EMPNO, e2.ENAME, e2.JOB, e2.SAL
+FROM emp e1
+JOIN emp e2 ON e1.SAL = e2.SAL
+WHERE e1.ENAME = 'SCOTT'
+  AND e2.ENAME != 'SCOTT';
 
