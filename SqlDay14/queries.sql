39b2809853c508ad3c86bc8fc1344fe48e5bdfdd
harshit001-2023 <harshitpande938@gmail.com>
2025-11-14 16:40:08 +0530

    Completing JOIN queries day 2 [13/11/2025]

--- SqlDay14/queries.sql
+++ SqlDay14/queries.sql
@@ -1,6 +1,25 @@
+--1. Write a query to display the employee names and their manager names
+--    from emp table by using self join(V.V.I.M.P)
+SELECT * FROM
+EMP E INNER JOIN EMP A
+ON E.ENAME = A.ENAME;
 
-
-SELECT e.EMPNO, e.ENAME, e.JOB, e.SAL, e.DEPTNO
+--2.Write a query to display the employee records who are reporting
+--   to BLAKE as a manager by using self join from emp table?
+SELECT e.EMPNO, e.ENAME, e.JOB, e.SAL, e.HIREDATE, e.DEPTNO
 FROM emp e
 JOIN emp m ON e.MGR = m.EMPNO
 WHERE m.ENAME = 'BLAKE';
+
+--3.Write a query to display the employees who are getting more salary
+--   than their manager salary from emp table using self join?
+SELECT E.ENAME FROM
+EMP E INNER JOIN EMP A
+ON E.MGR = A.EMPNO
+WHERE E.SAL > A.SAL;
+
+--4.Write a query to display BLAKE'S manager name from emp table
+--  by using self join?
+
+
+
